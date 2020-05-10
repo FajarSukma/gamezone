8dd0f0cc920c7fb0584cab4c2f8d2359b15c1965
FajarSukma <fajarsukma7@gmail.com>
2020-05-10 12:59:00 +0700

    Learn add image in React native, Add image to review detail gamezone

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,6 +1,6 @@
 import  React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, View, Text, Image} from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({navigation}) {
@@ -9,16 +9,31 @@ export default function ReviewDetails({navigation}) {
         navigation.goBack();
     }
 
-
+    const rating = navigation.getParam('rating')
+    console.log('RATINGGG : ' + rating)
     return(
         <View style={globalStyles.container}>
             <Card>            
             <Text>{ navigation.getParam('title')}</Text> 
             <Text>{ navigation.getParam('body')}</Text> 
-            <Text>{ navigation.getParam('rating')}</Text>     
+            {/* <Text>{ navigation.getParam('rating')}</Text>*/}
+            <View style={styles.rating}>
+                <Text>Game Zone Rating : </Text>
+                <Image source={images.rating[rating]}/>
+            </View>
             </Card>
         </View>
     )
 }
 
+
+const styles = StyleSheet.create({
+    rating:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop:16,
+        marginTop:16,
+        borderTopWidth:1,
+    }
+})
 
